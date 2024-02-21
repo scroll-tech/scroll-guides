195e6194695ccb7ecd29731124987cce01d55762
Emre <72754835+0xemrekaya@users.noreply.github.com>
2024-02-21 19:14:46 +0300

    Update deploy.ts, use 'target' instead of 'address'

--- contract-deploy-demo/scripts/deploy.ts
+++ contract-deploy-demo/scripts/deploy.ts
@@ -12,8 +12,8 @@ async function main() {
   });
   await lock.waitForDeployment();
 
-  console.log(`Lock with 0.00000001 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-  console.log(`Block explorer URL: https://blockscout.scroll.io/address/${lock.address}`);
+  console.log(`Lock with 0.00000001 ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`);
+  console.log(`Block explorer URL: https://blockscout.scroll.io/address/${lock.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
